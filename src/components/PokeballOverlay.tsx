12eac5d19b5dbd1a66c1903c0de5b956d05b60b2
Tomas Klement <tom.klemn@gmail.com>
2025-02-18 22:09:40 +0100

    hot fix overlay

--- src/components/PokeballOverlay.tsx
+++ src/components/PokeballOverlay.tsx
@@ -35,13 +35,13 @@ function PokeballOverlay() {
 
 
     
-        <div className={`fixed top-0 left-0 w-full h-1/2 bg-red-500 z-50 transition-transform duration-1000 ${opened ? "-translate-y-130" : "translate-y-0"}`}>
+        <div className={`fixed top-0 left-0 w-full h-1/2 bg-red-500 z-50 transition-transform duration-1000 ${opened ? "-translate-y-200" : "translate-y-0"}`}>
           <div className="absolute bottom-0 left-0 w-full h-[3px] bg-black" />
           <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 w-32 h-32 bg-white border-16 border-black rounded-full" />
         </div>
 
 
-        <div className={`fixed bottom-0 left-0 w-full h-1/2 bg-white z-40 transition-transform duration-1000 ${opened ? "translate-y-130" : "translate-y-0"}`}>
+        <div className={`fixed bottom-0 left-0 w-full h-1/2 bg-white z-40 transition-transform duration-1000 ${opened ? "translate-y-200" : "translate-y-0"}`}>
           <div className="absolute top-0 left-0 w-full h-[5px] bg-black" />
           <div className="absolute top-8 left-1/2 -translate-x-1/2 -translate-y-1/2 w-32 h-16 bg-gray-500 border-5 border-black rounded-b-full" />
         </div>
